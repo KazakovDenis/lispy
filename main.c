19e167caa8aa7d1a2d93151eea2eea0eeb859997
Denis Kazakov <d.kazakov@mcplat.ru>
2022-05-13 22:01:24 +0300

    add eval functions

--- main.c
+++ main.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "mpc/mpc.h"
 
 /* for Windows */
@@ -29,6 +26,37 @@ void add_history(char* unused) {}
 #endif
 
 
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  return 0;
+}
+
+
+long eval(mpc_ast_t* t) {
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+  
+  /* The operator is always second child. */
+  char* op = t->children[1]->contents;
+  
+  /* We store the third child in `x` */
+  long x = eval(t->children[2]);
+  
+  /* Iterate the remaining children and combining. */
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+  
+  return x;  
+}
+
+
 int main(int argc, char** argv) {
   mpc_parser_t* Number   = mpc_new("number");
   mpc_parser_t* Operator = mpc_new("operator");
@@ -37,41 +65,37 @@ int main(int argc, char** argv) {
 
   mpca_lang(
     MPCA_LANG_DEFAULT,
-    "                                                     \
-      number   : /-?[0-9]+/ ;                             \
-      operator :  '+'  |  '-'  |  '*'  | '/' | '%'        \
-               | \"add\" | \"sub\" | \"mul\" | \"div\" ;  \
-      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-      yalang   : /^/ <operator> <expr>+ /$/ ;             \
+    "                                                        \
+      number   : /-?[0-9]+(.[0-9]+)?/ ;                      \
+      operator : '+' | '-' | '*' | '/' | '%' ;               \
+      expr     : <number>                                    \
+               | <number> <operator> <number>                \
+               | '(' <number> (<operator> <number>)* ')' ;   \
+      yalang   : /^/ <expr> (<operator> <expr>)* /$/ ;       \
     ",
     Number, Operator, Expr, Yalang
   );
-  mpc_result_t r;
 
   puts("Yalang v0.0.1");
   puts("Press Ctrl+C to Exit\n");
 
-  while (1) {
-    char* input = readline("yalang> ");
-    if (input[0] == '\0') {
-      free(input);
-      continue;
-    }
-
+while (1) {
+    char* input = readline(">>> ");
     add_history(input);
 
+    mpc_result_t r;
     if (mpc_parse("<stdin>", input, Yalang, &r)) {
-      /* On Success Print the AST */
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
-    } else {
-      /* Otherwise Print the Error */
+    } else {    
       mpc_err_print(r.error);
       mpc_err_delete(r.error);
     }
+
     free(input);
   }
-
+  
   mpc_cleanup(4, Number, Operator, Expr, Yalang);
   return 0;
 }
